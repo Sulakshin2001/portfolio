7c3d7c2e1da1221d8e646b5677d9a3f6bc86c989
Sulakshin2001 <nikitasulaaskin@gmail.com>
2022-08-10 22:58:55 +0300

    part2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import './App.module.css';
 import {Header} from "./Header/Header";
 import {Main} from "./Main/Main";
 import {Skills} from "./skills/Skills";
 import {Work} from "./Work/Work";
 import {Contact} from "./contact/Contact";
 import {Footer} from "./Footer/Footer";
+import s from './App.module.css'
 
 function App() {
     return (
-        <div className="App">
+        <div className={s.App}>
             <Header/>
             <Main/>
             <Skills/>
